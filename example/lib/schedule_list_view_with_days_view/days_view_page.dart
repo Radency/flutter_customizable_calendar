fe9d36b4226a3ead8bc4009f2a3102e144b882cb
AndriiVivcharenko <andrii.v@radency.com>
2024-01-25 14:02:48 +0200

    added all-day events to the DaysView example

--- example/lib/schedule_list_view_with_days_view/days_view_page.dart
+++ example/lib/schedule_list_view_with_days_view/days_view_page.dart
@@ -1,4 +1,5 @@
 import 'package:example/colors.dart';
+import 'package:example/common/event_with_label/all_day_event_with_label.dart';
 import 'package:example/common/event_with_label/event_label.dart';
 import 'package:example/common/event_with_label/event_with_label.dart';
 import 'package:example/common/event_with_label/events_with_label_cubit.dart';
@@ -68,6 +69,28 @@ class _DaysViewPageState extends State<DaysViewPage> {
                     onEventUpdated: (event) {
                       widget.eventsCubit.updateEvent(event);
                     },
+                    allDayEventsTheme: const AllDayEventsTheme(
+                        listMaxRowsVisible: 2,
+                        eventHeight: 32,
+                        backgroundColor: Colors.white,
+                        containerPadding: EdgeInsets.zero,
+                        eventPadding:
+                            const EdgeInsets.symmetric(horizontal: 4.0),
+                        eventMargin: EdgeInsets.zero,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 2.0),
+                        borderRadius: 0,
+                        elevation: 0,
+                        alwaysShowEmptyRows: true,
+                        shape: BeveledRectangleBorder(),
+                        showMoreButtonTheme: AllDayEventsShowMoreButtonTheme(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 4.0,
+                            vertical: 2.0,
+                          ),
+                          padding: EdgeInsets.zero,
+                          height: 24,
+                        )),
                     timelineTheme: TimelineTheme(
                       timeScaleTheme: TimeScaleTheme(
                         textStyle: const TextStyle(
@@ -92,194 +115,212 @@ class _DaysViewPageState extends State<DaysViewPage> {
                         },
                       ),
                     ),
-                    monthPickerBuilder: (
-                      BuildContext context,
-                      DateTime focusedDate,
-                      List<CalendarEvent> events,
-                    ) {
-                      return Padding(
-                        padding: const EdgeInsets.only(
-                          left: 16.0,
-                        ),
-                        child: SizedBox(
-                          width: double.maxFinite,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  Navigator.of(context).pop();
-                                },
-                                borderRadius: BorderRadius.circular(4),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(2.0),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Icon(
-                                        Icons.keyboard_arrow_left,
-                                        color: ExampleColors.swatch24(),
-                                      ),
-                                      const SizedBox(width: 4),
-                                      Text(
-                                        "Back",
-                                        style: TextStyle(
-                                          color: ExampleColors.swatch24(),
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                    daysListBuilder: (
-                      context,
-                      focusedDate,
-                      events,
-                    ) {
-                      final filteredEvents = events.where((event) {
-                        return event.start.day == focusedDate.day &&
-                            event.start.month == focusedDate.month &&
-                            event.start.year == focusedDate.year;
-                      }).toList();
-                      return Column(
-                        children: [
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              const SizedBox(
-                                width: 24,
-                              ),
-                              Text(
-                                DateFormat.d().format(focusedDate),
-                                style: const TextStyle(
-                                  fontSize: 48,
-                                  fontWeight: FontWeight.bold,
-                                  height: 1,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 8,
-                              ),
-                              Text(
-                                DateFormat.EEEE()
-                                    .format(focusedDate)
-                                    .substring(0, 3),
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w400,
-                                  color: ExampleColors.black.withOpacity(.5),
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 24,
-                              ),
-                              Text(
-                                "${filteredEvents.length} events",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
-                                  color: ExampleColors.black.withOpacity(.5),
-                                ),
-                              ),
-                              Spacer(),
-                              InkWell(
-                                onTap: () {
-                                  widget.onAddClicked(
-                                    context,
-                                    focusedDate,
-                                  );
-                                },
-                                borderRadius: BorderRadius.circular(8),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: ExampleColors.swatch24(),
-                                    borderRadius: BorderRadius.circular(8),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: ExampleColors.black
-                                            .withOpacity(0.25),
-                                        blurRadius: 4,
-                                        spreadRadius: 1,
-                                        offset: const Offset(0, 0),
-                                      ),
-                                    ],
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: Icon(
-                                      Icons.add_outlined,
-                                      color: ExampleColors.white,
-                                      size: 36,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 24,
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                        ],
-                      );
-                    },
-                    eventBuilders: {
-                      EventWithLabel: (context, data) {
-                        final event = data as EventWithLabel;
-
-                        return Row(
-                          children: [
-                            Container(
-                              width: 4,
-                              decoration: BoxDecoration(
-                                color: event.label.color,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(4),
-                                  bottomLeft: Radius.circular(4),
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                height: double.maxFinite,
-                                decoration: BoxDecoration(
-                                  color: event.label.color.withOpacity(0.25),
-                                  borderRadius: BorderRadius.circular(4),
-                                ),
-                                padding: const EdgeInsets.all(8),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      event.title,
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      }
-                    },
+                    monthPickerBuilder: _buildMonthPicker,
+                    daysListBuilder: _buildDaysList,
+                    eventBuilders: _getEventsBuilder(),
                   );
                 },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Map<Type, EventBuilder<CalendarEvent>> _getEventsBuilder() {
+    return {
+      EventWithLabel: (context, data) {
+        return _buildEventWithLabel(data);
+      },
+      AllDayEventWithLabel: (context, data) {
+        return _buildEventWithLabel(
+          data,
+          allDay: true,
+        );
+      },
+    };
+  }
+
+  Row _buildEventWithLabel(
+    CalendarEvent data, {
+    bool allDay = false,
+  }) {
+    final event = data as EventWithLabel;
+
+    return Row(
+      children: [
+        Container(
+          width: 4,
+          decoration: BoxDecoration(
+            color: event.label.color,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(4),
+              bottomLeft: Radius.circular(4),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              color: event.label.color.withOpacity(0.25),
+              borderRadius: BorderRadius.circular(allDay ? 2 : 4),
+            ),
+            padding: EdgeInsets.all(allDay ? 2 : 8),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  event.title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDaysList(
+    context,
+    focusedDate,
+    events,
+  ) {
+    final filteredEvents = events.where((event) {
+      return event.start.day == focusedDate.day &&
+          event.start.month == focusedDate.month &&
+          event.start.year == focusedDate.year;
+    }).toList();
+    return Column(
+      children: [
+        const SizedBox(
+          height: 16,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(
+              width: 24,
+            ),
+            Text(
+              DateFormat.d().format(focusedDate),
+              style: const TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                height: 1,
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              DateFormat.EEEE().format(focusedDate).substring(0, 3),
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color: ExampleColors.black.withOpacity(.5),
+              ),
+            ),
+            const SizedBox(
+              width: 24,
+            ),
+            Text(
+              "${filteredEvents.length} events",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: ExampleColors.black.withOpacity(.5),
+              ),
+            ),
+            Spacer(),
+            InkWell(
+              onTap: () {
+                widget.onAddClicked(
+                  context,
+                  focusedDate,
+                );
+              },
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: ExampleColors.swatch24(),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: ExampleColors.black.withOpacity(0.25),
+                      blurRadius: 4,
+                      spreadRadius: 1,
+                      offset: const Offset(0, 0),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Icon(
+                    Icons.add_outlined,
+                    color: ExampleColors.white,
+                    size: 36,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 24,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMonthPicker(
+    BuildContext context,
+    DateTime focusedDate,
+    List<CalendarEvent> events,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16.0,
+      ),
+      child: SizedBox(
+        width: double.maxFinite,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              borderRadius: BorderRadius.circular(4),
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.keyboard_arrow_left,
+                      color: ExampleColors.swatch24(),
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      "Back",
+                      style: TextStyle(
+                        color: ExampleColors.swatch24(),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
